ad00cf74cd907aefd39b06efdd3aa7b58cad2887
karanpiplani1996 <157226274+karanpiplani1996@users.noreply.github.com>
2024-01-21 16:32:28 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 1234
+let accountEmail="karan@gmail.com"
+var accountPwd="12345"
+//accountID=112
+
+/* Prefer not to use var
+becasue of issue in block scope and functional*/
+
+console.log(accountID);
+console.table([accountEmail,accountPwd]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict" //treat all JS code as newer version
+//alert(3+3) //we are using nodejs, not browser
+console.log(3+3)
+console.log("Karan")
+
+let name = "Karan"
+let age=27
+let isLoggedin=false
+
+//number => 2 to power 53
+//bigint
